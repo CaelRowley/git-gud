3d31766864421a5cf42da1e7ed23f26264e28ea1
Cael Rowley <drdeus@pm.me>
2023-04-18 00:26:08 +0200

    feat: read command line args to give ferris to say

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
+struct Cli {
+    command: String,
+    argument: String,
+}
+
 fn main() {
+    let command = std::env::args().nth(1).expect("no command given");
+    let argument = std::env::args().nth(2).expect("no argument given");
+    
     let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
+    let message = command.as_str().to_owned() + " " + argument.as_str();
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_str(), width, &mut writer).unwrap();
+    say(&message, width, &mut writer).unwrap();
 }
