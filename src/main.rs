b62dcdbdcf50a5cd92974398dd62f2e6539b7f41
Cael Rowley <drdeus@pm.me>
2023-04-18 21:47:04 +0200

    feat: use git2 to clone a repo thats read from cli args

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
+use clap::Parser;
+use git2::Repository;
 
-struct Cli {
+#[derive(Parser)]
+struct Args {
     command: String,
     argument: String,
 }
 
 fn main() {
-    let command = std::env::args().nth(1).expect("no command given");
-    let argument = std::env::args().nth(2).expect("no argument given");
-    
-    let stdout = stdout();
-    let message = command.as_str().to_owned() + " " + argument.as_str();
-    let width = message.chars().count();
+   let args = Args::parse();
+   if args.command == "clone" {
+        let url = args.argument;
+        let _repo = match Repository::clone(&url, "./test") {
+            Ok(repo) => repo,
+            Err(e) => panic!("failed to clone {}", e),
+        };
+   }
+    // let stdout = stdout();
+    // let message = command.clone() + " " + &argument;
+    // let width = message.chars().count();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
-}
+    // let mut writer = BufWriter::new(stdout.lock());
+    // say(&message, width, &mut writer).unwrap();
+} 
