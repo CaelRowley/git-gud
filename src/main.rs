2a79d73baa5305166daee79dc35ecdfc326ab777
Cael Rowley <drdeus@pm.me>
2023-04-19 22:41:49 +0200

    feat: add a status command that shows changed and untracked files

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // use ferris_says::say;
 // use std::io::{stdout, BufWriter};
 use clap::Parser;
-use git2::Repository;
+use git2::{Repository, StatusOptions};
 
 #[derive(Parser)]
 struct Args {
@@ -10,14 +10,29 @@ struct Args {
 }
 
 fn main() {
-   let args = Args::parse();
-   if args.command == "clone" {
+    let args = Args::parse();
+    if args.command == "clone" {
         let url = args.argument;
         let _repo = match Repository::clone(&url, "./test") {
             Ok(repo) => repo,
             Err(e) => panic!("failed to clone {}", e),
         };
-   }
+    }
+    if args.command == "status" {
+        let repo = match Repository::open(".") {
+            Ok(repo) => repo,
+            Err(e) => panic!("no repo in current dir {}", e),
+        };
+        let mut opts = StatusOptions::new();
+        opts.include_untracked(true);
+        let statuses = match repo.statuses(Some(&mut opts)) {
+            Ok(statuses) => statuses,
+            Err(e) => panic!("oops {}", e),
+        };
+        for entry in statuses.iter() {
+            println!("{}", entry.path().unwrap());
+        }
+    }
     // let stdout = stdout();
     // let message = command.clone() + " " + &argument;
     // let width = message.chars().count();
