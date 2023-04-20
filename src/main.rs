ce4c4a45ef54d52d0a0bbc3b280c5ecf942cbe93
Cael Rowley <drdeus@pm.me>
2023-04-20 20:33:20 +0200

    feat: add git status command and terminal colours

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-// use ferris_says::say;
-// use std::io::{stdout, BufWriter};
+use ferris_says::say;
+use std::io::{stdout, BufWriter};
+use std::path::PathBuf;
+use colored::*;
 use clap::Parser;
 use git2::{Repository, StatusOptions};
+
 
 #[derive(Parser)]
 struct Args {
@@ -11,32 +14,105 @@ struct Args {
 
 fn main() {
     let args = Args::parse();
+
     if args.command == "clone" {
         let url = args.argument;
-        let _repo = match Repository::clone(&url, "./test") {
+        let repo = match Repository::clone(&url, "./test") {
             Ok(repo) => repo,
-            Err(e) => panic!("failed to clone {}", e),
+            Err(e) => panic!("Failed to clone repo: {}", e),
         };
+
+        println!("Repo cloned to: {}\n", repo.path().parent().unwrap().to_string_lossy().bold());
     }
+
     if args.command == "status" {
         let repo = match Repository::open(".") {
             Ok(repo) => repo,
-            Err(e) => panic!("no repo in current dir {}", e),
+            Err(e) => panic!("No repo in current dir: {}", e),
         };
+
         let mut opts = StatusOptions::new();
         opts.include_untracked(true);
+
         let statuses = match repo.statuses(Some(&mut opts)) {
             Ok(statuses) => statuses,
             Err(e) => panic!("oops {}", e),
         };
+        println!("On branch: {}\n", repo.head().unwrap().name().unwrap().to_string().bold());
+
+        let mut staged = vec![];
+        let mut unstaged = vec![];
+        let mut untracked = vec![];
+        let mut deleted = vec![];
+        let mut unkown = vec![];
+        
         for entry in statuses.iter() {
-            println!("{}", entry.path().unwrap());
+            let path = entry.path().unwrap().to_owned();
+
+            let status = entry.status();
+            if status.is_index_new() || status.is_index_modified() {
+                staged.push(path.clone());
+            } 
+            if status.is_wt_modified() {
+                unstaged.push(path);
+            } else if status.is_wt_new() {
+                untracked.push(path);
+            } else if status.is_wt_deleted() {
+                deleted.push(path);
+            }
+            else {
+                if !staged.contains(&path) {
+                    unkown.push(path);
+                }
+            }
+        }
+
+        if !staged.is_empty() {
+            println!("{}", "Changes to be committed:".bold().green());
+            for path in staged {
+                let path_buf = PathBuf::from(path);
+                println!("{}", format!("  new file: {}", path_buf.display()).green());
+            }
+            println!();
+        }
+        if !unstaged.is_empty() {
+            println!("{}", "Changes not staged for commit:".bold().yellow());
+            for path in unstaged {
+                let path_buf = PathBuf::from(path);
+                println!("{}", format!("  modified: {}", path_buf.display()).yellow());
+            }
+            println!();
+        }
+        if !untracked.is_empty() {
+            println!("{}", "Untracked files:".bold().red());
+            for path in untracked {
+                let path_buf = PathBuf::from(path);
+                println!("{}", format!("  {}", path_buf.display()).red());
+            }
+            println!();
+        }
+        if !deleted.is_empty() {
+            println!("{}", "Deleted files:".bold().red());
+            for path in deleted {
+                let path_buf = PathBuf::from(path);
+                println!("{}", format!("  {}", path_buf.display()).red());
+            }
+            println!();
+        }
+        if !unkown.is_empty() {
+            println!("{}", "Unkown files:".bold());
+            for path in unkown {
+                let path_buf = PathBuf::from(path);
+                println!("{}", format!(" {}", path_buf.display()))
+            }
+            println!();
         }
     }
-    // let stdout = stdout();
-    // let message = command.clone() + " " + &argument;
-    // let width = message.chars().count();
 
-    // let mut writer = BufWriter::new(stdout.lock());
-    // say(&message, width, &mut writer).unwrap();
+    let stdout = stdout();
+    let message = "Good git!";
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(&message, width, &mut writer).unwrap();
 } 
