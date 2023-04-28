549681d0921a60217cdea9172b2725aa4d8ceb30
Cael Rowley <drdeus@pm.me>
2023-04-28 22:24:06 +0200

    feat: add sync command

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::env;
 mod clone;
 mod status;
 mod push;
+mod sync;
 mod default;
 
 
 fn main() {
     let cli_args: Vec<String> = env::args().collect();
-
     let command = cli_args[1].clone();
 
     let mut args = Vec::new();
@@ -20,6 +20,7 @@ fn main() {
         "clone" | "c" => clone::clone(args),
         "status" | "s" => status::status(args),
         "push" | "p" => push::push(args),
+        "sync" => sync::sync(args),
         _ => default::default(args),
     }
 }
